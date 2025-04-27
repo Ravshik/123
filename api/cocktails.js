94141bc55ff02ce9fe920b10d48bea6f53ecb0f3
Ravshik <ravsan98@gmail.com>
2025-04-27 10:22:23 +0300

    Update cocktails.js

--- api/cocktails.js
+++ api/cocktails.js
@@ -1,19 +1,19 @@
 export default async function handler(req, res) {
-  const apiUrl = 'https://app.nocodb.com/api/v1/db/data/v1/Bar_lh/cocktails_xs_pack_csv';
-  const token = 'jyNETGfqOagPeIyErcVr7TZDdzkgbxZ2dGSDjLzQ';
+    const apiUrl = 'https://app.nocodb.com/api/v1/db/data/v1/cocktails_xs_pack_csv';
+    const token = 'jyNETgfoQagPelyErcVr7TDZdkgbxZZdG5DjLzQ'; // твой токен
 
-  try {
-    const response = await fetch(apiUrl, {
-      headers: {
-        'accept': 'application/json',
-        'xc-token': token,
-      },
-    });
+    try {
+        const response = await fetch(apiUrl, {
+            headers: {
+                'accept': 'application/json',
+                'xc-token': token,
+            },
+        });
 
-    const data = await response.json();
+        const data = await response.json();
 
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ message: 'Ошибка при получении данных', error: error.message });
-  }
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ message: 'Ошибка при получении данных', error: error.message });
+    }
 }
